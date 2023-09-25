9f46ddc8d66051212ef318c3e3d7756111384419
NadeebaAtiqui <nadeebaatiqui@gmail.com>
2023-09-25 17:22:30 -0500

    pulling

--- share_sched/lib/main.dart
+++ share_sched/lib/main.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:share_sched/firebase_options.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized;
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
