21ccb5431428e3a29f986fbd0a7006dae7442a22
faizankalam <kalamfaizan@gmail.com>
2023-09-26 10:46:45 -0500

    New User Register Screen (profile pic, full name, email, password, year)

---
+++ android/app/src/main/kotlin/com/example/new_user_screen_sharesched/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.new_user_screen_sharesched
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,187 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.blue[15]
+      ),
+      home: EditProfileUI(),
+    );
+  }
+}
+
+class EditProfileUI extends StatefulWidget {
+  @override
+  _EditProfileUIState createState() => _EditProfileUIState();
+}
+
+class _EditProfileUIState extends State<EditProfileUI> {
+  bool isObscurePassword = true;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Create New Profile'),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.settings,
+              color: Colors.white,
+            ),
+            onPressed: () {}
+          )
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 15, top: 20, right: 15),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Center(
+                child: Stack (
+                  children: [
+                    Container(
+                      width: 130,
+                      height: 130,
+                      decoration: BoxDecoration(
+                        border: Border.all(width: 4, color: Colors.grey),
+                        boxShadow: [
+                          BoxShadow(
+                            spreadRadius: 2,
+                            blurRadius: 10,
+                            color: Colors.black.withOpacity(0.1)
+                          )
+                        ],
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            'https://static.vecteezy.com/system/resources/thumbnails/005/544/770/small/profile-icon-design-free-vector.jpg'
+                          )
+                        )
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 4,
+                              color: Colors.white
+                            ),
+                              color: Colors.blue
+                          ),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+
+                        )
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 30),
+              buildTextField("Full Name", "..........", false),
+              buildTextField("Email", "..........", false),
+              buildTextField("Password", "**********", true),
+              buildTextField("Current Year", "Freshman", false),
+              SizedBox(height: 30),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  OutlinedButton(
+                      onPressed: () {},
+                      child: Text("CANCEL", style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.black
+                      )),
+                      style: OutlinedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 50),
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+
+    ),
+                  ),
+                  ElevatedButton(
+                  onPressed: () {},
+                      child: Text("SAVE", style: TextStyle(
+                        fontSize: 15,
+                        letterSpacing: 2,
+                        color: Colors.white
+                      )),
+                    style: ElevatedButton.styleFrom(
+                      primary: Colors.blue,
+                      padding: EdgeInsets.symmetric(horizontal: 50),
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20))
+                    ),
+                  )
+
+                ],
+              )
+
+            ],
+          ),
+        )
+      ),
+    );
+  }
+
+  Widget buildTextField(String labelText, String placeholder, bool isPasswordTextField) {
+    return Padding(
+      padding: EdgeInsets.only(bottom: 30),
+      child: TextField(
+        obscureText: isPasswordTextField ? isObscurePassword: false,
+        decoration: InputDecoration(
+          suffixIcon: isPasswordTextField ?
+              IconButton(
+                icon: Icon(Icons.remove_red_eye, color: Colors.grey),
+                onPressed: () {
+                  setState(() {
+                    isObscurePassword = !isObscurePassword;
+                  });
+                }
+              ): null,
+            contentPadding: EdgeInsets.only(bottom: 5),
+            labelText: labelText,
+              floatingLabelBehavior: FloatingLabelBehavior.always,
+              hintText: placeholder,
+              hintStyle: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey
+              )
+
+        ),
+      ),
+
+    );
+  }
+}
+
+
