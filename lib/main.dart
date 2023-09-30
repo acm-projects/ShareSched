1666b9814ff61d41f976ce14677f73d47575b075
NoelEmmanuel <noel.emmanuel20@gmail.com>
2023-09-30 01:41:40 -0500

    Firebase Properly working with Android

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/pages/custom_widgets.dart';
 import 'pages/login.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  if(kIsWeb){
+    await Firebase.initializeApp(options: FirebaseOptions(apiKey: "AIzaSyDyfTuJF3ZoevNmHPgxUliOGnc4h1r1jQU", appId: "1:505501683807:web:2c8367825b42c634a98bea", messagingSenderId: "505501683807", projectId: "sharesched-firebase"));
+  }
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
