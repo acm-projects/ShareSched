837b63f7e29796b908ea3b5e7b46247b3e408e17
arcode35 <roshanaldrin@gmail.com>
2023-10-20 00:38:52 -0500

    Expanded on the schedule page. Users can cge.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\scr\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\noele\ShareSched"
+export "FLUTTER_ROOT=/home/xor/Desktop/flutter"
+export "FLUTTER_APPLICATION_PATH=/home/xor/Desktop/ShareSched"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,94 +1,21 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:time_planner/time_planner.dart';
 import 'package:myapp/colors/app_colors.dart';
 
+List<TimePlannerTask> tasks = {};
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Schedule();
+  void addTaskButtonPressed() {
+    print('Pressed add task button');
   }
-}
-
-class ScheduleForm extends StatelessWidget {
-  const ScheduleForm({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        SizedBox(height: 50),
-        Text('Schedule',
-            style: GoogleFonts.quicksand(
-              fontSize: 32,
-              color: AppColors.primaryTextColor,
-              fontWeight: FontWeight.bold,
-            )),
-        const SizedBox(height: 20),
-      ],
-    );
-  }
-}
-
-class AddFriendsField extends StatelessWidget {
-  const AddFriendsField({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 35),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          // Username Label
-          const Text(
-            '   SEARCH',
-            style: TextStyle(
-              fontFamily: 'Mulish',
-              fontWeight: FontWeight.w800,
-              fontSize: 13.0,
-              letterSpacing: 1.5,
-              height: 1.0,
-              color: AppColors.primaryTextColor,
-            ),
-          ),
-          const SizedBox(height: 5),
-          // Username Text Field
-          TextFormField(
-            keyboardType: TextInputType.emailAddress,
-            decoration: InputDecoration(
-              labelText: 'Add friends with their username!',
-              prefixIcon: const Icon(Icons.search),
-              filled: true,
-              fillColor: Colors.white,
-              // Rounded edges
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.black, width: 1.5),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: Colors.white, width: 2.0),
-                borderRadius: BorderRadius.circular(15.0),
-              ),
-            ),
-            onChanged: (String value) {
-              // Handle changes
-            },
-            validator: (String? value) {
-              if (value == null || value.isEmpty) {
-                return 'Please enter a username';
-              }
-              return null;
-            },
-          ),
-        ],
+    return Scaffold(
+      body: const Schedule(),
+      floatingActionButton: AddTaskButton(
+        onPressed: addTaskButtonPressed,
       ),
     );
   }
@@ -169,11 +96,11 @@ class _ScheduleState extends State<Schedule> {
             SizedBox(
               height: 6,
             ),
-            CustomText(text: '218'),
+            CustomText(text: 'SCI 2.420'),
             SizedBox(
               height: 6,
             ),
-            CustomText(text: 'SCI 2.420'),
+            CustomText(text: '218'),
           ],
         )),
   ];
@@ -222,6 +149,25 @@ class _ScheduleState extends State<Schedule> {
             dividerColor: const Color.fromRGBO(53, 51, 205, 1),
             borderRadius: const BorderRadius.all(Radius.circular(20.0))),
         use24HourFormat: false,
+      ),
+    );
+  }
+}
+
+class AddTaskButton extends StatelessWidget {
+  final Function onPressed;
+  const AddTaskButton({super.key, required this.onPressed});
+
+  Widget build(BuildContext build) {
+    return ElevatedButton(
+      onPressed: () => onPressed(),
+      style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.buttonColor2,
+          shape: const CircleBorder(),
+          fixedSize: const Size(60.0, 60.0)),
+      child: const Icon(
+        Icons.add,
+        color: Colors.white,
       ),
     );
   }
